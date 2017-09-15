e1924381844a16e2d7d71dc2e1557576212bd8c2
cescoferraro <francescoaferraro@gmail.com>
2017-09-15 12:45:38 -0300

    eclim working

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,14 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import com.amazonaws.*;
 
 public class Application extends Controller {
+
     public static Result index() {
-        return ok("Hello world");
+
+        return ok("Hello");
     }
 
-    public static Result Cesco() {
-		System.out.println(45444);
-        return ok("33");
-    }
 
 }
