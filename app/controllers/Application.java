49e638e9c0cdd06d54ecc4f78d62aaadeb830374
cescoferraro <francescoaferraro@gmail.com>
2017-09-18 16:09:41 -0300

    brigadeiro

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,30 +1,30 @@
 package controllers;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import play.mvc.Controller;
-import play.mvc.Result;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.util.List;
 import java.util.Random;
+import play.mvc.Controller;
+import play.mvc.Result;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class Application extends Controller {
 
-    public static Result kitty() {
-        AWSCredentials credentials = new BasicAWSCredentials("AKIAJ7OWUPJ7ANREIBVA", "th8UJkoydxYnCtX/fqh8TdPLU7EOtIIykQ8OKNLb");
-        AmazonS3 s3client = new AmazonS3Client(credentials);
-        return redirect(Hey(s3client));
-    }
-
-  public static Result index() {
+	public static Result root() {
 		return ok("heyey");
 	}
 
+    public static Result kitty() {
+		BasicAWSCredentials creds = new BasicAWSCredentials("AKIAJ7OWUPJ7ANREIBVA", "th8UJkoydxYnCtX/fqh8TdPLU7EOtIIykQ8OKNLb"); 
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+			.withRegion("sa-east-1") .withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+        return redirect(Hey(s3Client));
+    }
+
 	private static String Hey(AmazonS3 s3client){
     	String bucket_name = "cescocats";
-        System.out.println("sdfjhsd");
         List<S3ObjectSummary> objects = s3client.listObjects(bucket_name).getObjectSummaries();
 		return s3client.generatePresignedUrl(bucket_name, objects.get(new Random().nextInt(objects.size())).getKey(), null).toString();
 	}
