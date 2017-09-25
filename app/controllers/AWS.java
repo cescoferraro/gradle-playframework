2cc8aae143cf86bdfa92b4e3a4b4c5278d00d148
cescoferraro <francescoaferraro@gmail.com>
2017-09-24 21:42:34 -0300

    ppsfajsf

---
+++ app/controllers/AWS.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.util.List;
+import java.util.Random;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import play.Configuration;
+
+public class AWS {
+	public AmazonS3 s3Client;
+	public List<S3ObjectSummary> photos;
+    Configuration configuration;
+	public String bucket_name = "cescocats";
+	
+	public AWS(Configuration configuration){
+		String AS = configuration.getString("aws.access.key");
+		String AK = configuration.getString("aws.secret.key");
+		BasicAWSCredentials creds = new BasicAWSCredentials(AS, AK); 
+		this.s3Client = AmazonS3ClientBuilder.standard()
+			.withRegion("sa-east-1") 
+			.withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+		this.photos = s3Client.listObjects(this.bucket_name).getObjectSummaries();
+	}
+	
+	public S3ObjectSummary random(){
+		return this.photos.get(new Random().nextInt(this.photos.size()));
+	}
+
+	public String randomCatURL(){
+		return this.s3Client
+			.generatePresignedUrl(this.bucket_name, this.random().getKey(), null).toString();
+
+	}
+}
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,35 +9,34 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 import javax.inject.Inject;
 import play.Configuration;
-import play.Logger;
 
 public class Application extends Controller {
 	AmazonS3 s3Client;
 	List<S3ObjectSummary> photos;
-    Configuration configuration;
+	Configuration configuration;
 	String bucket_name = "cescocats";
 
 	@Inject
 	public Application(Configuration config) {
-        this.configuration = config;
-		BasicAWSCredentials creds = new BasicAWSCredentials(configuration.getString("aws.access.key"), configuration.getString("aws.secret.key")); 
-		this.s3Client = AmazonS3ClientBuilder.standard()
-			.withRegion("sa-east-1") .withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+		this.configuration = config;
+		BasicAWSCredentials creds = new BasicAWSCredentials(configuration.getString("aws.access.key"),
+				configuration.getString("aws.secret.key"));
+		this.s3Client = AmazonS3ClientBuilder.standard().withRegion("sa-east-1")
+				.withCredentials(new AWSStaticCredentialsProvider(creds)).build();
 		this.photos = s3Client.listObjects(this.bucket_name).getObjectSummaries();
-    }
-
-	public Result root() {
-		return ok("sdkfjndsf");
 	}
 
-    public Result kitty() {
-		return redirect(this.s3Client.generatePresignedUrl(this.bucket_name, this.photos.get(new Random().nextInt(this.photos.size())).getKey(), null).toString());
-    }
+	public Result root() {
+		return ok(this.bucket_name);
+	}
 
-	// private String Hey(AmazonS3 s3client){
-    // 	String bucket_name = "cescocats";
-	// 	return s3client.generatePresignedUrl(bucket_name, this.photos.get(new Random().nextInt(this.photos.size())).getKey(), null).toString();
-	// }
+	public Result cat() {
+		System.out.println("sdkjfnskdf");
+		return redirect(this.s3Client.generatePresignedUrl(this.bucket_name,
+				this.photos.get(new Random().nextInt(this.photos.size())).getKey(), null).toString());
+	}
+
 }
